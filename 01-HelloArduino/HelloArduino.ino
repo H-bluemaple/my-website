5d21993a5e7ee3df2d416e19762b93e060739b49
H-bluemaple <carischen0313@gmail.com>
2025-12-08 20:16:14 +0800

    Create HelloArduino.ino

---
+++ 01-HelloArduino/HelloArduino.ino
@@ -0,0 +1,13 @@
+cpp
+void setup() {
+  // put your setup code here, to run once:这里写“准备做什么”
+    Serial.begin(9600);//初始化串口通信，串口监视器波特率必须与此一致
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:这里写重复做什么
+    Serial.println("Hello Arduino");//打印字符
+    delay(1000);                    //delay函数控制重复频率，1000ms后打印一条新的字符//
+    Serial.println("Do you want to play with me?");
+    delay(2000);
+}
