27b68dcae5ddff1bd26f4f47ea77a4b36b57e46a
H-bluemaple <carischen0313@gmail.com>
2026-01-29 20:27:07 +0800

    Add files via upload

---
+++ Arduino-Learning/04-SevenSegment-CommonCathode/code/02_all_segments_on/02_all_segments_on.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(2, OUTPUT); // a
+  pinMode(3, OUTPUT); // b
+  pinMode(4, OUTPUT); // c
+  pinMode(5, OUTPUT); //d
+  pinMode(6, OUTPUT); //e
+  pinMode(7, OUTPUT); //f
+  pinMode(8, OUTPUT); //g
+  pinMode(9, OUTPUT); //dp
+
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH); 
+}
+
+void loop() {}
